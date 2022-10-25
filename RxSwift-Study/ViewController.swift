6b38be948a0dc56b1e41c67abd27e0078cc14808
Raphael Martin <Raphael.Martin.-ND@disney.com>
2022-10-25 16:29:28 -0300

    Created login page

--- RxSwift-Study/ViewController.swift
+++ RxSwift-Study/ViewController.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Username"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
+    }()
+    
+    private var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
+    }()
+    
+    private var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        
+        return button
+    }()
+    
+    private var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
+    private let horizontalMargin = 20.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        setupHierarchy()
+        setupConstraints()
     }
 
-
+    private func setupHierarchy() {
+        [usernameTextField,
+         passwordTextField,
+         loginButton]
+            .forEach { stackView.addArrangedSubview($0) }
+        
+        view.addSubview(stackView)
+    }
+    
+    private func setupConstraints() {
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: horizontalMargin).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -horizontalMargin).isActive = true
+    }
 }
 
