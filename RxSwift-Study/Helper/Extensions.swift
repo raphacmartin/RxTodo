64ae52d74f754e1734277e4bef091ac9ed7458eb
Raphael Martin <raphaelcastromartin@gmail.com>
2023-03-07 10:25:29 -0300

    Refactored and documented `flatMapSorted(:)` extension function

--- RxSwift-Study/Helper/Extensions.swift
+++ RxSwift-Study/Helper/Extensions.swift
@@ -17,21 +17,29 @@ extension Date {
 }
 
 extension Observable {
+    ///
+    /// Projects each element of an observable sequence to an observable sequence and merges the resulting observable sequences into one observable sequence, ensuring that concurrent executions of observable sequences happens, but producing values only from the most recent observable sequence.
+    ///
+    /// - parameter initialValue: A value to be compared with first emitted event
+    /// - parameter selector: A transform function to apply to each element.
+    /// - returns: An observable sequence whose elements are the result of invoking the one-to-many transform function on each element of the input sequence.
     func flatMapSorted<Source: ObservableConvertibleType>(initialValue: Source.Element, _ selector: @escaping (Element) throws -> Source)
     -> Observable<Source.Element> {
+        typealias IndexedEvent = (index: Int, value: Source.Element)
+        
         return self
             .enumerated()
-            .flatMap { index, element -> Observable<(Int, Source.Element)> in
+            .flatMap { index, element -> Observable<IndexedEvent> in
                 return try! selector(element)
                     .asObservable()
                     .map { element in
-                        return (index, element)
+                        return IndexedEvent(index: index, value: element)
                     }
             }
-            .scan((-1, initialValue), accumulator: { prev, next in
+            .scan((0, initialValue), accumulator: { prev, next in
                 // return the event with the higher index
-                return [prev, next].max { a, b in a.0 < b.0 }!
+                next.index >= prev.index ? next : prev
             })
-            .map { $0.1 }
+            .map { $0.value }
     }
 }
